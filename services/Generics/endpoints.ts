5f490153128a30cace0d454ba6a312271d4059f0
GonzaloEBaez <gezbaez@gmail.com>
2025-11-07 13:02:53 -0300

    endpoint para porcentaje perfil

--- services/Generics/endpoints.ts
+++ services/Generics/endpoints.ts
@@ -72,6 +72,7 @@ export const ENDPOINTS = {
     UPLOAD_CV: `/Candidato/upload_cv`,
     UPLOAD_FOTO_PERFIL: `/Candidato/upload_foto_perfil`,
     VERIFICAR_PERFIL: `/Candidato/verificar_perfil`,
+    GET_PORCENTAJE_PERFIL: `/Candidato/get_porcentaje_perfil`,
     COMPLETAR_PERFIL: `/Candidato/completar_perfil`,
   },
   EMPRESA: {
